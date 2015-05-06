54ad9208ebb4517f4812ecda8140413c2f38a4c0
Giau Ngo <giaunv@gmail.com>
2015-05-06 18:16:40 +0700

    Add Unit Tests

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,14 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String index(){
-        return "Grettings from Spring Boot!";
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
+
 }
